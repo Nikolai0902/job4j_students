5f33dd87c57c5815f96edbb8640709ed626b96bb
Nikolai0902 <kbus94@yandex.ru>
2024-10-21 00:55:29 +0300

    Add serv_s: repository, MinioConfig

---
+++ services/serv_s/src/main/java/ru/serv_s/configuration/MinioConfig.java
@@ -0,0 +1,34 @@
+package ru.serv_s.configuration;
+
+import io.minio.MinioClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Настройка клиента для взаимодействия с MinIO.
+ */
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    public String minioUrl;
+
+    @Value("${minio.access.key}")
+    public String accessKey;
+
+    @Value("${minio.access.secret}")
+    public String secretKey;
+
+    @Value("${minio.port}")
+    public int port;
+
+    @Bean
+    public MinioClient minioClient() {
+        return new MinioClient.Builder()
+                .credentials(accessKey, secretKey)
+                .endpoint(minioUrl, port, false)
+                .build();
+    }
+}
